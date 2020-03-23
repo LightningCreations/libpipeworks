c3e6359333d4e5002dc775ca0a9cb1be720477c6
Ray Redondo <rdrpenguin04@gmail.com>
2020-03-23 10:00:43 -0500

    Initialize state.things and create add_thing

--- src/engine.c
+++ src/engine.c
@@ -2,5 +2,9 @@
 #include "engine.h"
 
 void init_engine() {
-    
+    pw_engine_state.things = NULL; // STRETCHY BUFFER
+}
+
+void add_thing(pw_thing *thing) {
+    sb_push(pw_engine_state.things, thing);
 }
--- src/engine.h
+++ src/engine.h
@@ -2,5 +2,5 @@
 #include "stretchy_buffer.h"
 
 struct pw_engine_state {
-    pw_thing *things; // STRETCHY BUFFER
+    pw_thing **things; // STRETCHY BUFFER, vector of (pw_thing *)
 } pw_engine_state;
