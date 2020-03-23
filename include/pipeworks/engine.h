f4dd9bac697c48bc15a97136eaf67a58ba4f9978
Ray Redondo <rdrpenguin04@gmail.com>
2020-03-23 11:25:36 -0500

    Add more engine functions

--- include/pipeworks/engine.h
+++ include/pipeworks/engine.h
@@ -1,12 +1,24 @@
 #ifndef PIPEWORKS_ENGINE_H
 #define PIPEWORKS_ENGINE_H
 
+#include "game.h"
 #include "thing.h"
 
 typedef struct pw_engine pw_engine;
 
+// Creation/destruction
 pw_engine* pw_init_engine();
-void add_thing(pw_engine*, pw_thing*);
 void pw_destroy_engine(pw_engine*);
+
+// Threading
+void pw_start(pw_engine*);
+void pw_join(pw_engine*);
+void pw_stop(pw_engine*);
+
+// Configuring
+void pw_set_game(pw_engine*, pw_game*);
+void pw_engine_add_thing(pw_engine*, pw_thing*); // To distinguish from pw_canvas_add_thing
+void pw_engine_clear_things(pw_engine*); // To distinguish from pw_canvas_clear_things
+void pw_engine_remove_thing(pw_engine*, pw_thing*); // To distinguish from pw_canvas_remove_thing
 
 #endif
---
+++ include/pipeworks/game.h
@@ -0,0 +1,6 @@
+#ifndef PIPEWORKS_GAME_H
+#define PIPEWORKS_GAME_H
+
+typedef struct pw_game pw_game;
+
+#endif
