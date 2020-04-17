77e74c73ca2acf6f246a1a546be480d75a9fa0bf
Ray Redondo <rdrpenguin04@gmail.com>
2020-04-16 22:42:23 -0500

    Fix a segfault and add a test window

--- pipeworks/src/engine.c
+++ pipeworks/src/engine.c
@@ -7,6 +7,7 @@
 
 // Non-pipeworks lib includes
 #include "ll.h"
+#include <SDL.h>
 
 // Pipeworks includes
 #include <pipeworks/thing.h>
@@ -16,6 +17,30 @@ pw_engine* pw_init_engine()
     pw_engine *result = malloc(sizeof(pw_engine));
     result->things = ll_init();
     return result;
+}
+
+void pw_internal_start0(void *_engine)
+{
+    pw_engine *engine = (pw_engine*) _engine;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("TODO: Add config for window name", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
+
+    SDL_Delay(3000); // TODO: Add game loop
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+}
+
+void pw_start(pw_engine *engine)
+{
+    engine->thread = SDL_CreateThread(pw_internal_start0, "Pipeworks Engine Thread", engine);
+}
+
+void pw_join(pw_engine *engine)
+{
+    SDL_WaitThread(engine->thread, NULL);
 }
 
 void pw_engine_add_thing(pw_engine *engine, pw_thing *thing)
--- test_game/src/main.c
+++ test_game/src/main.c
@@ -37,4 +37,9 @@ int main()
     pw_set_load_state(game, game_load_state, (void*)(&game_data));
 
     pw_set_game(engine, game);
+
+    pw_start(engine);
+    pw_join(engine);
+
+    pw_destroy_engine(engine);
 }
