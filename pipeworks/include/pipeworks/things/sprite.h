95d13b79d0b3f6c2d65ae9ac0237b972b0de2f37
Ray Redondo <rdrpenguin04@gmail.com>
2020-07-10 13:19:00 -0500

    AAAAAAAA

--- pipeworks/include/pipeworks/things/sprite.h
+++ pipeworks/include/pipeworks/things/sprite.h
@@ -1,31 +1,33 @@
 #ifndef PW_THINGS_SPRITE_H
 #define PW_THINGS_SPRITE_H
 
+#include "../pw-std.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct pw_sprite pw_sprite;
 
-pw_sprite* pw_init_sprite(float x, float y, float width, float height, float depth, uint32_t numFrames, const char *const *const frames); // All the consts
+PIPEWORKS_API pw_sprite* pw_init_sprite(float x, float y, float width, float height, float depth, uint32_t numFrames, const char *const *const frames); // All the consts
 
-void pw_sprite_add_update_handler(void(*handler)(pw_sprite *sprite));
+PIPEWORKS_API void pw_sprite_add_update_handler(void(*handler)(pw_sprite *sprite));
 
-void pw_sprite_set_x(float x);
-void pw_sprite_set_y(float y);
-float pw_sprite_get_x();
-float pw_sprite_get_y();
-void pw_sprite_set_pos(float x, float y);
+PIPEWORKS_API void pw_sprite_set_x(float x);
+PIPEWORKS_API void pw_sprite_set_y(float y);
+PIPEWORKS_API float pw_sprite_get_x();
+PIPEWORKS_API float pw_sprite_get_y();
+PIPEWORKS_API void pw_sprite_set_pos(float x, float y);
 
-void pw_sprite_set_width(float width);
-void pw_sprite_set_height(float height);
-float pw_sprite_get_width();
-float pw_sprite_get_height();
-void pw_sprite_set_size(float width, float height);
+PIPEWORKS_API void pw_sprite_set_width(float width);
+PIPEWORKS_API void pw_sprite_set_height(float height);
+PIPEWORKS_API float pw_sprite_get_width();
+PIPEWORKS_API float pw_sprite_get_height();
+PIPEWORKS_API void pw_sprite_set_size(float width, float height);
 
-void pw_sprite_set_pos_and_size(float x, float y, float width, float height);
+PIPEWORKS_API void pw_sprite_set_pos_and_size(float x, float y, float width, float height);
 
-void pw_sprite_set_frame(uint32_t id);
+PIPEWORKS_API void pw_sprite_set_frame(uint32_t id);
 
 #ifdef __cplusplus
 }
