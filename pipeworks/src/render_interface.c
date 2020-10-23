a750b83cfc9c0be17da0a169add3707f831d7d7e
Ray Redondo <rdrpenguin04@gmail.com>
2020-10-23 12:33:55 -0500

    `inline`, how could you...

--- pipeworks/src/render_interface.c
+++ pipeworks/src/render_interface.c
@@ -1,21 +1,21 @@
 #include <pipeworks/render_interface.h>
 
-inline void pw_ri_plot_pixel(pw_render_interface render, uint32_t x, uint32_t y, uint32_t color) {
+void pw_ri_plot_pixel(pw_render_interface render, uint32_t x, uint32_t y, uint32_t color) {
     render.vtable->plot_pixel(render.data, x, y, color);
 }
 
-inline uint32_t pw_ri_get_min_x(pw_render_interface render) {
+uint32_t pw_ri_get_min_x(pw_render_interface render) {
     return render.vtable->get_min_x(render.data);
 }
 
-inline uint32_t pw_ri_get_max_x(pw_render_interface render) {
+uint32_t pw_ri_get_max_x(pw_render_interface render) {
     return render.vtable->get_max_x(render.data);
 }
 
-inline uint32_t pw_ri_get_min_y(pw_render_interface render) {
+uint32_t pw_ri_get_min_y(pw_render_interface render) {
     return render.vtable->get_min_y(render.data);
 }
 
-inline uint32_t pw_ri_get_max_y(pw_render_interface render) {
+uint32_t pw_ri_get_max_y(pw_render_interface render) {
     return render.vtable->get_max_y(render.data);
 }
